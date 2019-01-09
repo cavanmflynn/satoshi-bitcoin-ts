17a04b25dcb879f9bca6c094d851342e1dd58585
Cavan <cavan@radarrelay.com>
2019-01-09 10:36:51 -0700

    Allow number to be returned a big number instance

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ const conversion = '100000000';
 /**
  * Convert Satoshi to Bitcoin
  * @param satoshi Amount of Satoshi to convert. Must be a whole number
+ * @param returnBN Optionally return the value as a Big number instance
  */
-export function toBitcoin(satoshi: number | string) {
+export function toBitcoin(satoshi: number | string, returnBig: boolean = false) {
   if (!isString(satoshi) && !isNumber(satoshi)) {
     throw new TypeError(
       `toBitcoin must be called on a number or string, got ${typeof satoshi}`,
@@ -16,18 +17,25 @@ export function toBitcoin(satoshi: number | string) {
   if (!Number.isInteger(Number(satoshi))) {
     throw new TypeError('toBitcoin must be called on a whole number or string format whole number');
   }
+  if (returnBig) {
+    return new Big(satoshi).div(conversion);
+  }
   return Number(new Big(satoshi).div(conversion));
 }
 
 /**
  * Convert Bitcoin to Satoshi
  * @param bitcoin Amount of Bitcoin to convert
+ * @param returnBN Optionally return the value as a Big number instance
  */
-export function toSatoshi(bitcoin: number | string) {
+export function toSatoshi(bitcoin: number | string, returnBig: boolean = false) {
   if (!isString(bitcoin) && !isNumber(bitcoin)) {
     throw new TypeError(
       `toSatoshi must be called on a number or string, got ${typeof bitcoin}`,
     );
+  }
+  if (returnBig) {
+    return new Big(bitcoin).times(conversion);
   }
   return Number(new Big(bitcoin).times(conversion));
 }
